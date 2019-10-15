bd9ec071973bedc8bf602817561e190480ff8267
alonelzb <1531554877@qq.com>
2019-10-15 21:08:53 +0800

    nwe add

--- merge_split_pdf.py
+++ merge_split_pdf.py
@@ -15,16 +15,9 @@ def pdf_split(pdf):
     with open(output_filename, 'wb') as f:
         pdf_writer.write(f)       
 
-def get_info(pdf):
-    pdf_reader = PdfFileReader(f)
-    with open(pdf, 'rb') as f:
-        info = pdf_reader.getDocumentInfo()
-        pages = pdf_reader.getNumPages()
-    print(info)
-    print(pages)
-
 def merge_pdf(pdfs):
     pdf_writer = PdfFileWriter()
+    print(pdfs)
     for pdf in pdfs:
         pdf_reader = PdfFileReader(pdf)
         for page in range(pdf_reader.getNumPages()):
@@ -33,25 +26,40 @@ def merge_pdf(pdfs):
     with open('merge.pdf', 'wb') as f:
         pdf_writer.write(f)
 
+    print(f'合并成功 merge.pdf')
+
 def custom_split(pdf):
     pdf_reader = PdfFileReader(pdf)
     pdf_writer = PdfFileWriter()
     page_count = pdf_reader.getNumPages()
-    print(page_count)
-    for page in range(3):
-        pdf_writer.addPage(pdf_reader.getPage(page))
-    with open('custom.pdf', 'wb') as f:
-        pdf_writer.write(f)
-
-    i = PdfFileReader('custom.pdf').getNumPages()
-    print(i)
+    print(f'一共{page_count} 页')
+    try:
+        section = input('输入分割起始页数(以空格分开):')
+        start = int(section.split()[0])
+        end = int(section.split()[1])
+        for page in range(start, end+1):
+            pdf_writer.addPage(pdf_reader.getPage(page))
+        name = pdf.split('.')[0]
+        with open(f'{name}_{start}_{end}.pdf', 'wb') as f:
+            pdf_writer.write(f)
+    except:
+        return print('error!')
+    # i = PdfFileReader('custom.pdf').getNumPages()
+    # print(i)
+    print('success!')
 
 
 if __name__ == '__main__':
-    # path = 'info.pdf'
-    # pdfs = ['info_3.pdf', 'info_4.pdf']
-    # pdf_split(path)
-    # get_info(path)
-    # merge_pdf(pdfs)
-    custom_split('info.pdf')
+    print('1.分割pdf  2.合并pdf')
+    option = input()
+    if option == '1':
+        pdf = input('输入当前文件夹下需要处理的PDF:\n')
+        custom_split(pdf)
+    elif option == '2':
+        pdfs = input('输入需要合并的pdf:')
+        pdfs = pdfs.split()
+        merge_pdf(pdfs)
+    else:
+        print('输入错误！')
+
 
